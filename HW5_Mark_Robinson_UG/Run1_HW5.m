2a4caec596508d10d991ee88509e37e44c6691ae
mdrobinson4 <mdrobinson4@crimson.ua.edu>
2019-10-23 18:07:35 -0500

    Clean homework

--- HW5_Mark_Robinson_UG/Run1_HW5.m
+++ HW5_Mark_Robinson_UG/Run1_HW5.m
@@ -9,7 +9,7 @@ load("264_optdigits.mat");
 % split data between training and testing
 [trainlabels,trainfeatures,testlabels,testfeatures] = splitData(class_label,data);
 % grid search iteration count
-n = 10;
+n = 20;
 % find the best cost and gamma values
 [c,g,a,params] = gridSearch(trainlabels,trainfeatures,n);
 % train the model using the best cost and gamma
@@ -33,25 +33,25 @@ end
 
 % perform grid search to get the best parameters
 % try different cost and gamma values
-function [c,g,accuracy,params] = gridSearch(trainlabels, trainfeatures,n)
-max = [0,0,0];
-for c = linspace(-10.8198,35.2319,n)
-    c = exp(c);
-    for g = linspace(-33.8456,7.6009,n)
-        g = exp(g);
-        params = sprintf('-s 0 -t 0 -c %d -v 3 -g %d -q', c,g);
-        curr = svmtrain(trainlabels, trainfeatures, params);
-        if curr > max(3)
-            max(1) = c;
-            max(2) = g;
-            max(3) = curr;
+function [cost,gamma,accuracy,params] = gridSearch(trainlabels, trainfeatures,n)
+optimalParams = [0,0,0];
+costArr = exp(linspace(-10.8198,35.2319,n));
+gammaArr = exp(linspace(-33.8456,7.6009,n));
+for cost = costArr
+    for gamma = gammaArr
+        params = sprintf('-s 0 -t 0 -c %d -v 4 -g %d -q', cost,gamma);
+        currAcc = svmtrain(trainlabels, trainfeatures, params);
+        if currAcc > optimalParams(3)
+            optimalParams(1) = cost;
+            optimalParams(2) = gamma;
+            optimalParams(3) = currAcc;
         end
     end
 end
-accuracy = max(3);
-c = max(1);
-g = max(2);
-params = sprintf('-s 0 -t 0 -c %d -g %d -q', c,g);
+accuracy = optimalParams(3);
+cost = optimalParams(1);
+gamma = optimalParams(2);
+params = sprintf('-s 0 -t 0 -c %d -g %d -q', cost,gamma);
 end
     
 function model = train(trainlabels,trainfeatures,params)
