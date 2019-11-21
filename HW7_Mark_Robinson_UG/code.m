8230c31523bb1eec246172a5a3ffbb2a49ede024
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-11-21 16:48:42 -0600

    added report

--- HW7_Mark_Robinson_UG/code.m
+++ HW7_Mark_Robinson_UG/code.m
@@ -3,40 +3,56 @@ load("hmm.mat");
 
 % addpath(genpath('\HMMall')) 
 
-%rng(sum('MarkRobinson'), 'default');
-
 % learn parameters
 [q1, prior1, transmat1, obsmat1, llVal1, LL1, loglik1] = estParams(data1, 3);
 [q2, prior2, transmat2, obsmat2, llVal2, LL2, loglik2] = estParams(data2, 3);
 
 % classify sequences
 X = [X1 ; X2 ; X3 ; X4 ; X5 ; X6];
-class = classify(X, prior1, prior2, transmat1, transmat2, obsmat1, obsmat2);
+[class, classProb] = classify(X, prior1, prior2, transmat1, transmat2, obsmat1, obsmat2);
 
 
-clc;
+clc; % clear the training details
 % show the results
+hold on;
 displayResults(1, LL1, llVal1, q1, prior1, obsmat1, transmat1, loglik1);
 displayResults(2, LL2, llVal2, q2, prior2, obsmat2, transmat2, loglik2);
+sgtitle('Logarithmic Likelihood of Sequence');
+hold off;
+
+classResults(class, classProb);
+
+function classResults(class, classProb)
+fprintf('----------------------------------------------\n');
+fprintf('|          Classification Results            |\n');
+fprintf('----------------------------------------------\n');
+%fprintf('\n------------------------------------------------------\n\n');
+for j=1:1:6
+    fprintf('Process %d generated sequence X%d\n', class(j), j);
+    fprintf('Logarithmic likelihood: %f\n\n\n', classProb(j));
+end
+%fprintf('------------------------------------------------------\n\n');
+end
 
 function displayResults(j, ll, llVal, q, prior, obsmat, transmat, loglik)  
-figure(j)
+subplot(1,2,j);
 plot(1:1:length(loglik), loglik);
-title('Logarithmic Likelihood of sequence')
+str = sprintf('Process: %d', j);
+title(str)
 ylabel('Log Likelihood')
 xlabel('Number of states')
 fprintf('---------------------------------\n');
 fprintf('|          Process: %d            |\n', j);
 fprintf('---------------------------------\n');
 fprintf('States: %d\nObservations: %d\n',q,3);
-fprintf('Initial Priors:\n');
+fprintf('Initial State Probabilites:\n');
 prior
 fprintf('Observation Probabilies:\n');
 obsmat
 fprintf('Transition Probablities:\n');
 transmat
 fprintf('Logarithmic Likelihood: %f\n', llVal);
-fprintf('---------------------------------\n');
+fprintf('\n\n\n');
 end
 
 
@@ -48,8 +64,8 @@ obsmat = {};
 loglik = [];
 LL = {};
 
-rng(sum('MarkRobinson'), 'twister');
-for q=1:1:20
+for q=1:1:40
+    rng(sum('MarkRobinson'), 'twister');
     prior{q} = normalise(rand(q,1));
     transmat{q} = mk_stochastic(rand(q,q));
     obsmat{q} = mk_stochastic(rand(q,obCount));
@@ -64,16 +80,19 @@ stateCount = I;
 LL = LL{q};
 end
 
-function [class] = classify(X, prior1, prior2, transmat1, transmat2, obsmat1, obsmat2)
+function [class, classProb] = classify(X, prior1, prior2, transmat1, transmat2, obsmat1, obsmat2)
 class = zeros(1,6);
+classProb = zeros(1,6);
 for i=1:1:6
     x = X(i,:);
     loglik1 = dhmm_logprob(x, prior1, transmat1, obsmat1);
     loglik2 = dhmm_logprob(x, prior2, transmat2, obsmat2);
     if loglik1 >= loglik2
         class(i) = 1;
+        classProb(i) = loglik1;
     else
         class(i) = 2;
+        classProb(i) = loglik2;
     end
 end
 end
